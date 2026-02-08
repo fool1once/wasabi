fa54c1aabb1122cff28414888c5c612800abe7ab
fool1once <jhonbenedictzulueta@gmail.com>
2026-02-08 17:04:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+
+noBtn.addEventListener("mouseenter", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
+function moveButton() {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 100 - 50;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div style="
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      height:100vh;
+      font-family:Arial;
+      background:#ffd6e0;
+    ">
+      <h1>YAY!! 💘🥰</h1>
+    </div>
+  `;
+});
 
